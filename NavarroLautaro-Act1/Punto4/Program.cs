eb506f0c20f16b4756992f5103274f8134584895
Lautaro-Navarro18 <lautaronavarro1818@gmail.com>
2026-03-27 11:49:50 -0300

    Add files via upload

---
+++ NavarroLautaro-Act1/Punto4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*4. Se debe desarrollar un programa que pida el ingreso del precio de un artículo y la cantidad que lleva el cliente. Mostrar lo que debe abonar el comprador.*/
+            int precio, cantidad, resultado;
+            string linea;
+            Console.Write("Ingrese el precio: ");
+            linea = Console.ReadLine();
+            precio = int.Parse(linea);
+            Console.Write("Ingrese la cantidad: ");
+            linea = Console.ReadLine();
+            cantidad = int.Parse(linea);
+
+            resultado = precio * cantidad;
+            Console.Write("tenes que pagar: ");
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NavarroLautaro-Act1/Punto5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*5. Realizar la carga del radio de un círculo, mostrar por pantalla la circunferencia y el área del mismo 
+             * (La circunferencia se calcula multiplicando el doble del radio por π (pi), y el área se calcula multiplicando π por el cuadrado del radio).*/
+            int radio, circunferencia, area;
+            string linea;
+            Console.Write("pon el radio: ");
+            linea = Console.ReadLine();
+            radio = int.Parse(linea);
+
+            circunferencia = 3 * (radio * 2);
+            area = 3 * (radio * radio);
+            Console.Write("La circunferencia es de: ");
+            Console.WriteLine(circunferencia);
+            Console.Write("El area es de: ");
+            Console.WriteLine(area);
+            Console.ReadKey();
+        }
+    }
+}
