25c328efff44c5443a1bb047a94c4a5cfd4b606b
Lautaro-Navarro18 <lautaronavarro1818@gmail.com>
2026-04-09 10:30:33 -0300

    Add files via upload

---
+++ NavarroLautaro-Act7/Punto1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    /*1. Desarrollar un programa que permita ingresar un vector de 8 elementos, e informe:
+El valor acumulado de todos los elementos del vector.
+El valor acumulado de los elementos del vector que sean mayores a 36.
+Cantidad de valores mayores a 50.*/
+    internal class Valores
+    {
+        private int[] numeros;
+        void CargarValores()
+        {
+            numeros = new int[8];
+            for(int i = 0; i < 8; i++)
+            {
+                Console.Write("coloque los valores: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+        }
+        void ValorAcumulado()
+        {
+            int suma = 0;
+            for(int i = 0; i < 8; i++)
+            {
+                suma = suma + numeros[i];
+            }
+            Console.Write("El valor acumulado de los valores es: " + suma + "\n");
+        }
+        void MayorQ36()
+        {
+            int mas = 0;
+            for(int i = 0; i < 8; i++)
+            {
+                if (numeros[i] > 36)
+                {
+                    mas++;
+                }
+            }
+            Console.Write("Hay " + mas + " numeros mas grandes que 36 \n");
+        }
+        void MayorQ50()
+        {
+            int mas = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if (numeros[i] > 50)
+                {
+                    mas++;
+                }
+            }
+            Console.Write("Hay " + mas + " numeros mas grandes que 50 \n");
+        }
+        static void Main(string[] args)
+        {
+            Valores v = new Valores();
+            v.CargarValores();
+            v.ValorAcumulado();
+            v.MayorQ36();
+            v.MayorQ50();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NavarroLautaro-Act7/Punto2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class SumaV
+    {
+        /*2. Realizar un programa que pida la carga de dos vectores numéricos enteros de 4
+elementos. Obtener la suma de los dos vectores, dicho resultado guardarlo en un
+tercer vector del mismo tamaño. Sumar componente a componente.*/
+        private int[] V1;
+        private int[] V2;
+        private int[] VS;
+        void CargarV()
+        {
+            V1 = new int[4];
+            V2 = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write("cargue los valores del primer vector: ");
+                V1[i] = int.Parse(Console.ReadLine());
+            }
+            for (int f = 0; f < 4; f++)
+            {
+                Console.Write("cargue los valores del segundo vector: ");
+                V2[f] = int.Parse(Console.ReadLine());
+            }
+        }
+        void Suma()
+        {
+            VS = new int[4];
+            for(int i = 0; i < 4; i++)
+            {
+                VS[i] = V1[i];
+                VS[i] = VS[i] + V2[i];            //el problema profe, de porque sumaba 20 sin motivo era porque esto: '\n' + VS[i] + '\n' lo lee como 10 + VS[i] + 10 
+                Console.Write("\n" + VS[i]);      //la solucion es poner le comillas dobles " en lugar de '
+            }
+        }
+        static void Main(string[] args)
+        {
+            SumaV sv = new SumaV();
+            sv.CargarV();
+            sv.Suma();
+            Console.ReadKey();
+        }
+    }
+}
