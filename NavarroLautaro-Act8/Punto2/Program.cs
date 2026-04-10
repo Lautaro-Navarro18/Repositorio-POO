c410d70cc328b1cee8d526f4f0548d70aacf12f8
Lautaro-Navarro18 <lautaronavarro1818@gmail.com>
2026-04-10 11:59:33 -0300

    Add files via upload

---
+++ NavarroLautaro-Act8/Punto2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    internal class Orden
+    {
+        /*2. Definir un vector donde almacenar los nombres de 5 países. Confeccionar el algoritmo de ordenamiento alfabético.*/
+        private string[] paises;
+        void CargarP()
+        {
+            Console.WriteLine("Escriba los paises: ");
+            for(int i = 0; i < paises.Length; i++)
+            {
+                paises[i] = Console.ReadLine();
+            }
+        }
+        void OrdenA()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Orden o = new Orden();
+            o.CargarP();
+            o.OrdenA();
+            Console.ReadKey();
+        }
+    }
+}
