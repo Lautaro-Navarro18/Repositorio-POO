12cdda0ab0980db38a76306371ec43aad70406de
Lautaro-Navarro18 <lautaronavarro1818@gmail.com>
2026-04-10 10:10:32 -0300

    Fix account number condition in main loop

--- NavarroLautaroAct4/Punto4/Program.cs
+++ NavarroLautaroAct4/Punto4/Program.cs
@@ -10,7 +10,8 @@ namespace Punto4
     {
         static void Main(string[] args)
         {
-            /*4. En un banco se procesan datos de las cuentas corrientes de sus clientes. De cada cuenta corriente se conoce: número de cuenta y saldo actual. El ingreso de datos debe
+            /*4. En un banco se procesan datos de las cuentas corrientes de sus clientes. De cada cuenta corriente se conoce: 
+             * número de cuenta y saldo actual. El ingreso de datos debe
              * finalizar al ingresar un valor negativo en el número de cuenta.
              * Se pide confeccionar un programa que lea los datos de las cuentas corrientes e informe:
              * a) De cada cuenta: número de cuenta y estado de la cuenta según su saldo, sabiendo
@@ -26,11 +27,11 @@ namespace Punto4
             {
                 Console.Write("\nIngrese el numero de cuenta: ");
                 numC = int.Parse(Console.ReadLine());
-                if(numC > 1)
+                if (numC > -1)
                 {
                     Console.Write("Ingrese el saldo actual: ");
                     salA = int.Parse(Console.ReadLine());
-                    if(salA > 0)
+                    if (salA > 0)
                     {
 
                         Console.Write("La cuenta N° " + numC + " es un Acreedor con un saldo de: " + salA);
@@ -38,7 +39,7 @@ namespace Punto4
                     }
                     else
                     {
-                        if(salA < 0)
+                        if (salA < 0)
                         {
                             Console.Write("La cuenta N° " + numC + " es un Deudor con saldo de: " + salA);
                         }
