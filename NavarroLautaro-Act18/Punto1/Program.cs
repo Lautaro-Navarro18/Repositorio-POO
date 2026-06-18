9cebffbd9bedad1477a3cfbee3f24bc8263863cf
Lautaro-Navarro18 <lautaronavarro1818@gmail.com>
2026-06-18 10:03:04 -0300

    Add files via upload

---
+++ NavarroLautaro-Act18/Punto1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    /*
+    1. Confeccionar una clase llamada DispositivoEnergia que tenga como atributos privados el CodigoIdentificador (string) y la GeneracionKwh (double, que representa los Kilowatts-hora
+    generados). Definir sus respectivas propiedades de lectura y escritura, validando que la generación no sea un valor negativo (en caso de serlo, asignarle 0). Plantear un método para
+    imprimir estos datos básicos. |
+    Luego, crear una segunda clase llamada PanelSolar que herede de DispositivoEnergia. Añadir un atributo propio privado llamado AreaMetros (double, que representa la superficie del panel
+    en metros cuadrados) con su propiedad correspondiente (validando que sea mayor a cero). |
+    Implementar un método para imprimir todos los datos del panel, incluyendo los heredados. |
+    En el programa principal (Main): 
+    ●Crear un objeto de la clase DispositivoEnergia, ingresar valores y probar su impresión.
+    ●Crear un objeto de la clase PanelSolar, cargar sus datos por consola y comprobar que puede acceder tanto a sus propiedades heredadas como a las propias para realizar la muestra de info.
+    */
+    public class DispositivoEnergia
+    {
+        private string codigoId;
+        private double genKwh;
+        public string RcodigoId
+        {
+            set
+            {
+                codigoId = value;
+            }
+            get
+            {
+                return codigoId;
+            }
+        }
+        public double RgenKwh
+        {
+            set
+            {
+                genKwh = value;
+            }
+            get
+            {
+                return genKwh;
+            }
+        }
+        public DispositivoEnergia()
+        {
+            Console.Write("Ingrese el codigo de identificacion del dispositivo: ");
+            codigoId = Console.ReadLine();
+            Console.Write("Ingrese la generacion por hora en Kwh: ");
+            genKwh = double.Parse(Console.ReadLine());
+            if(genKwh < 0)
+            {
+                genKwh = 0;
+            }
+        }
+        public void MostrarD()
+        {
+            Console.WriteLine("El dispositivo " + codigoId + " tiene una generacion por hora de " + genKwh + "Kwh");
+        }
+    }
+    public class PanelSolar : DispositivoEnergia
+    {
+        private double areaM;
+        public PanelSolar()
+        {
+            Console.Write("Ingrese el area en metros cuadrados del panel solar: ");
+            areaM = double.Parse(Console.ReadLine());
+            while (areaM < 0)
+            {
+                Console.WriteLine("Ingrese un valor valido: ");
+                areaM = double.Parse(Console.ReadLine());
+            }
+        }
+        public void MostrarDP()
+        {
+            Console.WriteLine("El dispositivo " + RcodigoId + " que produce " + RgenKwh + "Kwh tiene un area de " + areaM + "m²");
+        }
+        static void Main(string[] args)
+        {
+            DispositivoEnergia de = new DispositivoEnergia();
+            de.MostrarD();
+            PanelSolar ps = new PanelSolar();
+            ps.MostrarDP();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NavarroLautaro-Act18/Punto2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    /*
+    2.Plantear una clase llamada CriaturaMarina y otra clase llamada HabitatAcuatico.
+    La clase CriaturaMarina debe tener como atributos privados: Especie (string), ProfundidadOptima (int, en metros) y NivelSalinidad 
+    (un valor de 1 a 100). Definir las propiedades necesarias para acceder a estos atributos asegurando mediante validaciones que:
+    ● La profundidad óptima sea estrictamente mayor a cero (0). |
+    ● El nivel de salinidad se encuentre únicamente en el rango de 1 a 100 (de lo contrario, asignar un valor por defecto de 35, que representa la salinidad promedio del océano). |
+    La clase HabitatAcuatico debe contener como atributo un vector capaz de almacenar 3 objetos de la clase CriaturaMarina. Definir un método dentro de HabitatAcuatico 
+    para cargar las 3 criaturas y otro método para mostrar todas las criaturas ordenadas de menor a mayor en base a su profundidad óptima. Además, el programa debe 
+    informar la especie que requiere el mayor nivel de salinidad para sobrevivir.
+    */
+    public class CriaturaMarina
+    {
+        private string especie;
+        private int profundiO, nivelS;
+        public CriaturaMarina()
+        {
+            Console.Write("Ingrese la especie del animal: ");
+            especie = Console.ReadLine();
+            Console.Write("Ingrese la profundidad optima del mismo: ");
+            profundiO = int.Parse(Console.ReadLine());
+            if(profundiO < 0)
+            {
+                Console.WriteLine("Es un pez, no una gaviota");
+                profundiO = 5;
+            }
+            Console.Write("Ingrese el nivel de salinidad de la criatura: ");
+            nivelS = int.Parse(Console.ReadLine());
+            if (nivelS <0)
+            {
+                Console.WriteLine("Te falto un poco de sal para que quede rico 6/10");
+                nivelS = 35;
+            }
+            else if (nivelS > 100)
+            {
+                Console.WriteLine("¿Querias pez seco?");
+                nivelS = 35;
+            }
+        }
+        public string Respecie
+        {
+            set
+            {
+                especie = value;
+            }
+            get
+            {
+                return especie;
+            }
+        }
+        public int RprofundiO
+        {
+            set
+            {
+                profundiO = value;
+            }
+            get
+            {
+                return profundiO;
+            }
+        }
+        public int RnivelS
+        {
+            set
+            {
+                nivelS = value;
+            }
+            get
+            {
+                return nivelS;
+            }
+        }
+    }
+    internal class HabitadAcuatico
+    {
+        private CriaturaMarina[] epez = new CriaturaMarina[3];
+        public HabitadAcuatico()
+        {
+            for (int i = 0; i < epez.Length; i++)
+            {
+                epez[i] = new CriaturaMarina();
+            }
+        }
+        public void Ordenar()
+        {
+            int auxPO = 0;
+            for (int i = 0; i < epez.Length; i++)
+            {
+                for (int j = 0; j < epez.Length - 1; j++)
+                {
+                    if (epez[j].RprofundiO > epez[j + 1].RprofundiO)
+                    {
+
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+        }
+    }
+}
