3731d4b8ca07240158b4f2aa4d19bb9a4be6e7b6
Lautaro-Navarro18 <lautaronavarro1818@gmail.com>
2026-06-26 12:03:36 -0300

    Add files via upload

---
+++ NavarroLautaro-Act20/Punto1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto1
+{
+    /*
+    1: Sistema de control de vuelos 
+    Problema:
+    Una aerolínea administra los vuelos programados mediante un sistema orientado a objetos.
+    Plantear una clase base Vuelo que contenga como propiedades: número de vuelo, destino y duración en horas. Luego definir una clase derivada 
+    VueloInternacional que herede de Vuelo e incluya una propiedad adicional llamada paisDestino.
+    Crear una lista de objetos de tipo VueloInternacional, cargar 5 vuelos con sus datos desde consola y luego:
+     Mostrar los vuelos registrados y el país de destino.
+     Informar cuál es el vuelo con mayor duración.
+     Mostrar el orden de ejecución de los constructores. 
+    */
+    public class Vuelo
+    {
+        private int numroV, duracion;
+        private string destino;
+        public Vuelo(int numV, string dest, int dur)
+        {
+            numroV = numV;
+            destino = dest;
+            duracion = dur;
+        }
+        public int RnumroV
+        {
+            set
+            {
+                numroV = value;
+            }
+            get
+            {
+                return numroV;
+            }
+        }
+        public string Rdestino
+        {
+            set
+            {
+                destino = value;
+            }
+            get
+            {
+                return destino;
+            }
+        }
+        public int Rduracion
+        {
+            set
+            {
+                duracion = value;
+            }
+            get
+            {
+                return duracion;
+            }
+        }
+    }
+    public class VueloInternacional : Vuelo
+    {
+        string paisD;
+        public VueloInternacional(string paD, int numV, string dest, int dur) : base (numV, dest, dur)
+        {
+            paisD = paD;
+        }
+        public string RpaisD
+        {
+            set
+            {
+                paisD = value;
+            }
+            get
+            {
+                return paisD;
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<VueloInternacional> vuelo = new List<VueloInternacional>();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Ingrese el numero de vuelo: ");
+                int numV = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el destino: ");
+                string dest = Console.ReadLine();
+                Console.Write("Ingrese la duracion en horas: ");
+                int dur = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el pais del destino: ");
+                string paD = Console.ReadLine();
+                VueloInternacional nuevoV = new VueloInternacional(paD, numV, dest, dur);
+                vuelo.Add(nuevoV);
+                Console.WriteLine("Se ingreso correctamente la informacion");
+            }
+            foreach (VueloInternacional i in vuelo)
+            {
+                Console.WriteLine("El vuelo: " + i.RnumroV + " se dirige hacia " + i.Rdestino + " en el pais " + i.RpaisD + " el vuelo durara " + i.Rduracion + "h");
+            }
+            int auxV = vuelo[0].RnumroV, auxD = vuelo[0].Rduracion;
+            foreach (VueloInternacional i in vuelo)
+            {
+                if (auxD < i.Rduracion)
+                {
+                    auxD = i.Rduracion;
+                    auxV = i.RnumroV;
+                }
+            }
+            Console.WriteLine("El vuelo de mayor duracion fue el N°" + auxV + " con una duracion de " + auxD + "h");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ NavarroLautaro-Act20/Punto2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto2
+{
+    /*
+    Actividad 2: Administración de entregas logísticas
+    Problema:
+    Una empresa de envíos desea registrar sus entregas mediante colaboración de clases.
+    Crear una clase Paquete con propiedades: código, peso y destino. Crear una clase Despachador que contenga una lista de paquetes y un método para registrar nuevos paquetes.
+    Desde el método Main, instanciar un Despachador, cargar 5 paquetes y mostrar:
+     Todos los paquetes registrados.
+     Cuántos superan los 10 kg de peso.
+     Cuántos tienen destino nacional (por ejemplo, “Argentina”).
+    */
+    public class Paquete
+    {
+        int codigo, peso;
+        string destino;
+        public Paquete(int codi, int pe, string dest)
+        {
+            codigo = codi;
+            peso = pe;
+            destino = dest;
+        }
+        public int Rcodigo
+        {
+            set { codigo = value; }
+            get { return codigo; }
+        }
+        public int Rpeso
+        {
+            set { peso = value; }
+            get { return peso;}
+        }
+        public string Rdest
+        {
+            set { destino = value; }
+            get { return destino; }
+        }
+    }
+    internal class Despachador : Paquete
+    {
+        List<Despachador> despa = new List<Despachador>();
+        
+        public Despachador(int codi, int pe, string dest) : base(codi, pe, dest)
+        {
+            for (int i = 0; i < 2; i++) //CAMBIAR A 5  ===================================================================================================================================
+            {
+                Console.Write("Ingrese el codigo: ");
+                codi = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el peso: ");
+                pe = int.Parse(Console.ReadLine());
+                Console.Write("Ingrese el destino: ");
+                dest = Console.ReadLine();
+                Despachador newDespa = new Despachador(codi, pe, dest);
+                despa.Add(newDespa);
+            }
+        }
+        public void Mostrar()
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
