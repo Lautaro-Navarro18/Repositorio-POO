604d1e5be955c05e1bf916a4c7f61a5c9e4ae1a3
Lautaro-Navarro18 <lautaronavarro1818@gmail.com>
2026-07-09 16:34:28 -0300

    Update Program.cs

--- NavarroLautaro-Act22/Punto3/Program.cs
+++ NavarroLautaro-Act22/Punto3/Program.cs
@@ -1,89 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Punto3
 {
-     /*
-Actividad 3: Registro de entrenamientos
-Problema:
-Plantear una clase Entrenamiento con atributos: deportista y duración (en minutos).
- Incluir dos métodos RegistrarDuracion (sobrecarga de métodos):
-  1. Uno que reciba horas y minutos y los convierta a minutos.
-  2. Otro que reciba directamente los minutos.
- Crear una lista con 5 entrenamientos y mostrar el entrenamiento más largo y el más corto. 
-*/
- internal class Entrenamiento
- {
-     public string deportista;
-     public TimeSpan duracion;
-     public Entrenamiento()
-     {
-         Console.Write("Ingrese el nombre del deportista: ");
-         deportista = Console.ReadLine();
-         /*Console.Write("Como va a ingresar el tiempo de entrenamiento? horas y minutos (HM) o solo minutos (M)? ");
-         string opcion = Console.ReadLine();
-         if (opcion == "HM")
-         {
-             Console.Write("Ingrese la cantidad de horas: ");
-             int horas = int.Parse(Console.ReadLine());
-             Console.Write("Ingrese la cantidad de minutos: ");
-             int minutos = int.Parse(Console.ReadLine());
-             RegistrarDuracion(horas, minutos);
-         }
-         else if (opcion == "M")
-         {
-             Console.Write("Ingrese la cantidad de minutos: ");
-             int minutos = int.Parse(Console.ReadLine());
-             RegistrarDuracion(minutos);
-         }*/
-         Console.WriteLine("Insgrese el tiempo de entrenamiento");
-         TimeSpan te = TimeSpan.Parse(Console.ReadLine());
-         TimeSpan uh = new TimeSpan(1, 0, 0);
-         if(te > uh)
-         {
-             int horas = te.Hours;
-             int minutos = te.Minutes;
-             RegistrarDuracion(horas, minutos);
-         }
-         else
-         {
-             int minutos = te.Minutes;
-             RegistrarDuracion(minutos);
-         }
-     }
-     public void RegistrarDuracion(int horas, int minutos)
-     {
-         duracion = TimeSpan.FromMinutes((horas * 60) + minutos);
-     }
-     public void RegistrarDuracion(int minutos)
-     {
-         duracion = TimeSpan.FromMinutes(minutos);
-     }
-     static void Main(string[] args)
-     {
-         Entrenamiento[] entre = new Entrenamiento[3];
-         for (int i = 0; i < entre.Length; i++)
-         {
-             entre[i] = new Entrenamiento();
-         }
-         TimeSpan auxL = entre[0].duracion, auxC = entre[0].duracion;
-         for (int i = 0; i < entre.Length; i++)
-         {
-             if (auxL < entre[i].duracion)
-             {
-                 auxL = entre[i].duracion;
-             }
-             if (auxC > entre[i].duracion)
-             {
-                 auxC = entre[i].duracion;
-             }
-         }
-         Console.WriteLine("El entrenamiento más largo dura " + auxL.TotalMinutes + " minutos.");
-         Console.WriteLine("El entrenamiento más corto dura " + auxC.TotalMinutes + " minutos.");
-         Console.ReadKey();
-     }
- }
+    /*
+    Actividad 3: Registro de entrenamientos
+    Problema:
+    Plantear una clase Entrenamiento con atributos: deportista y duración (en minutos).
+      Incluir dos métodos RegistrarDuracion (sobrecarga de métodos):
+      1. Uno que reciba horas y minutos y los convierta a minutos.
+      2. Otro que reciba directamente los minutos.
+      Crear una lista con 5 entrenamientos y mostrar el entrenamiento más largo y el más corto. 
+    */
+    internal class Entrenamiento
+    {
+        private string deportista;
+        private TimeSpan duracion;
+        public void RegistrarDuracion(int horas, int minutos)
+        {
+            duracion = new TimeSpan(horas, minutos, 0);
+            Console.WriteLine(duracion + " en horas");
+        }
+        public void RegistrarDuracion(int minutos)
+        {
+            duracion = TimeSpan.FromMinutes(minutos);
+            Console.WriteLine(duracion + " minutos");
+        }
+        public Entrenamiento()
+        {
+            Console.Write("Ingresa el nombre del deportista: ");
+            deportista = Console.ReadLine();
+            Console.Write("Ingresa cuantos minutos duro su entrenamiento: ");
+            int minutos = int.Parse(Console.ReadLine());
+            RegistrarDuracion(minutos);
+        }
+        static void Main(string[] args)
+        {
+            Entrenamiento[] train = new Entrenamiento[5];
+
+            for (int i = 0; i < train.Length; i++)
+            {
+                train[i] = new Entrenamiento();
+            }
+            TimeSpan mayor = train[0].duracion;
+            int pos = 0;
+            TimeSpan menor = train[0].duracion;
+            for (int j = 0; j < train.Length; j++)
+            {
+                if (menor > train[j].duracion)
+                {
+                    menor = train[j].duracion;
+                    pos = j;
+                }
+            }
+            Console.WriteLine("El entrenamiento con menor duracion es de " + menor + " minutos ");
+            for (int k = 0; k < train.Length; k++)
+            {
+                if (train[k].duracion > mayor)
+                {
+                    mayor = train[k].duracion;
+                    pos = k;
+                }
+            }
+            Console.WriteLine("El entrenamiento con mayor duracion es de " + mayor + " minutos ");
+            Console.ReadKey();
+        }
+    }
 }
