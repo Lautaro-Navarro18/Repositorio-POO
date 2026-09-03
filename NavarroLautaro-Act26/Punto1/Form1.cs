c6aad4d1149fa474bf7c437c0d3386724bd455ff
Lautaro-Navarro18 <lautaronavarro1818@gmail.com>
2026-09-03 08:35:16 -0300

    Add files via upload

---
+++ NavarroLautaro-Act26/Punto1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto1
+{
+    /*
+    1. Disponer tres objetos de la clase CheckBox con nombres de navegadores web.
+    Cuando se presione un botón mostrar en el título del Form los programas seleccionados. 
+    */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Text = "";
+            if(checkBox1.Checked == true)
+            {
+                Text = Text += checkBox1.Text;
+            }
+            if (checkBox2.Checked == true)
+            {
+                Text = Text += checkBox2.Text;
+            }
+            if (checkBox3.Checked == true)
+            {
+                Text = Text += checkBox3.Text;
+            }
+        }
+    }
+}
---
+++ NavarroLautaro-Act26/Punto3/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto3
+{
+    /*
+    3. Solicitar el ingreso del nombre de una persona y seleccionar de un control ComboBox un país. Al presionar un botón mostrar en la barra del 
+    título del Form el nombre ingresado y el país seleccionado. 
+    */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Text = "";
+            Text = textBox1.Text + " " + comboBox1.Text;
+        }
+    }
+}
