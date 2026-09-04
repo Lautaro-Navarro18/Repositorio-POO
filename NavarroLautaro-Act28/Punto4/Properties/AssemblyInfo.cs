313129870cc57b3c20fc6c43704e993aa4a37cd9
Lautaro-Navarro18 <lautaronavarro1818@gmail.com>
2026-09-04 12:03:09 -0300

    Add files via upload

---
+++ NavarroLautaro-Act28/Punto4/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("Punto4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Escuela Confederacion Suiza")]
+[assembly: AssemblyProduct("Punto4")]
+[assembly: AssemblyCopyright("Copyright © Escuela Confederacion Suiza 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e5047729-a2c9-4451-a242-9c447d675f7a")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NavarroLautaro-Act28/Punto5/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace Punto5
+{
+    /*
+    Consigna: Cargar en un ComboBox tres opciones. Al cambiar la selección mediante el evento SelectedIndexChanged, 
+    mostrar la imagen correspondiente dentro del PictureBox. 
+    */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                pictureBox1.ImageLocation = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTia8OAO-yuBZHGXF6Fp8uv58R6DA55iL1vLtxB3ACvAA&s=10";
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                pictureBox1.ImageLocation = "https://www.diarioepoca.com/content/bucket/6/628376.jpg";
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                pictureBox1.ImageLocation = "https://i.ytimg.com/vi/_XlRbVNantc/maxresdefault.jpg";
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+    }
+}
